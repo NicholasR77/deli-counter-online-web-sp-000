c8ce97a9b4b205c0af4c5830e1eb32d97b5b6dc2
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 20:17:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
   katz_deli.each.with_index do |name, position|
     katz_deli_numbers.push("#{position + 1}. #{name}")
   end
-  
+
   if (katz_deli.length == 0)
     puts "The line is currently empty."
   else
@@ -30,6 +30,3 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
-
-line(katz_deli)
-take_a_number(katz_deli, "Nick")
