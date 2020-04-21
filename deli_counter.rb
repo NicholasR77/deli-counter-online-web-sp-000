3f260030614d6836f1a6a9cd0a287fba54221e16
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 20:17:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,12 @@ katz_deli = ["bob", "joe", "nathan"]
 katz_deli_numbers = []
 
 def line(katz_deli)
+  katz_deli_numbers = []
+
+  katz_deli.each.with_index do |name, position|
+    katz_deli_numbers.push("#{position + 1}. #{name}")
+  end
+  
   if (katz_deli.length == 0)
     puts "The line is currently empty."
   else
