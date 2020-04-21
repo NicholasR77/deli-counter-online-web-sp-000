d24d9f21c4a703128acf6b664780706de623474d
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 19:55:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,12 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  puts "Now serving #{katz_deli[0].}"
-  katz_deli.shift
+  if (katz_deli.length == 0)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Now serving #{katz_deli[0].}"
+    katz_deli.shift
+  end
 end
 
 take_a_number(katz_deli, "nick")
