c583202b3a0db267feb6694fb1789f1cb4e34319
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 19:50:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli, name)
   if (array.length == 0)
     puts "The line is currently empty."
   else
-    puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
   end
 end
 
