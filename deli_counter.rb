fb3dab2a2d5dc36615f7282f40187064bd9386f7
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 19:44:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
 
-line = ["bob", "joe", "nathan"]
+line = []
 
 def line(array, element)
   if (array.length == 0)
     puts "The line is currently empty."
   else
     puts "Your place in line is #{array.index(element)+1}"
-  end  
+  end
 end
 
 
