984f83966cf9cdd7258375042a81c6518d4d1c95
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 20:19:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are currently number #{katz_deli.index(name)}"
+  puts "Welcome, #{name}. You are currently number #{katz_deli.index(name)+1} in line."
 end
 
 def now_serving(katz_deli)
