2c5f401f3792690925585268a7e685cff696daa4
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-21 20:12:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
   if (katz_deli.length == 0)
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{katz_deli_numbers.join()}"
+    puts "The line is currently: #{katz_deli_numbers.join(" ")}"
   end
 end
 
